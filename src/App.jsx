379f323493d173db93f78ccdb268f13ed61dc86a
vicharatb <tharkana98@gmail.com>
2025-03-11 14:37:59 +0530

    New Day1 First

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
+  useEffect(() => {
+    console.log("rtert");
+  }, []);
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return <></>;
+  return (
+    <>
+      <h1>Hello, React Hooks!</h1>
+    </>
+  );
 }
+
+/*
+const fetchData = async () => {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const [count, setCount] = useState(0);
+const handleIncrement = () => {
+  setCount(prev => prev + 1);
+};
+
+return (
+  <>
+    <button onClick={handleIncrement}>Increment</button>
+    <p>{count}</p>
+  </>
+);
+*/
 
 export default App;
